517169c8173e2c5cf158cde5eb14776f2a2746a5
Pratik Salunke <pratiksalunke3112@gmail.com>
2026-04-23 23:33:15 +0530

    product page's hero section done

--- frontend/src/landing_page/products/ProductPage.jsx
+++ frontend/src/landing_page/products/ProductPage.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import Hero from '../home/Hero'
-import LeftSection from './LeftSection'
-import RightSection from './RightSection'
-import Universe from './Universe'
+import React from "react";
 
+import Hero from "./Hero";
+import LeftSection from "./LeftSection";
+import RightSection from "./RightSection";
+import Universe from "./Universe";
 
-function ProductPage() {
+function ProductsPage() {
   return (
     <>
-    <Hero/>
-    <LeftSection />
-    <RightSection />
-    <Universe/>
+      <Hero />
+
+      <LeftSection
+        imageURL="media/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <RightSection />
+
+      <Universe />
     </>
-  )
+  );
 }
 
-export default ProductPage
+export default ProductsPage;
